9dce6f10c8f1c9b482252f74339b6fa957223021
King Maxwell <maxwellmaitland97@gmail.com>
2023-07-26 00:51:46 -0400

    %msg%

---
+++ push.ps1
@@ -0,0 +1,5 @@
+git add *
+set /p "msg=Enter Commit Message: "
+git commit -a -m "%msg%"
+git push
+curl http://192.168.254.156:8081/job/Deploy%20Genso/build?token=11ba6d22f6ab9b626f186094f6afb9f243
