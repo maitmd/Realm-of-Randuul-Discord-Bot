7a0bb91dc631f390209aa20c45111d341183cb94
King Maxwell <maxwellmaitland97@gmail.com>
2023-07-26 00:15:10 -0400

    t

--- push.bat
+++ push.bat
@@ -2,4 +2,4 @@ git add *
 set /p "msg=Enter Commit Message: "
 git commit -a -m "%msg%"
 git push
-curl "http://192.168.254.156:8082/job/Deploy%20Genso/build?token=xokorjpbqfzjfhfglhtwimcpxgdqrv"
+curl "http://192.168.254.156:8081/job/Deploy%20Genso/build?token=xokorjpbqfzjfhfglhtwimcpxgdqrv"
