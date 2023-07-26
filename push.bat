4453f5a20be1a626aacb48b7d3b754885eec43c5
King Maxwell <maxwellmaitland97@gmail.com>
2023-07-26 00:33:00 -0400

    testing remeote build

--- push.bat
+++ push.bat
@@ -2,4 +2,4 @@ git add *
 set /p "msg=Enter Commit Message: "
 git commit -a -m "%msg%"
 git push
-curl "http://192.168.254.156:8081/job/Deploy%20Genso/build?token=xokorjpbqfzjfhfglhtwimcpxgdqrv?username=Emanix?password=Nutty!11123"
+curl "http://192.168.254.156:8081/job/Deploy%20Genso/build?token=11ba6d22f6ab9b626f186094f6afb9f243
