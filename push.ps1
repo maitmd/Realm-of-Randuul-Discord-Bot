7bb4e3eee36ee460c2448accd73aee293b05a7d6
King Maxwell <maxwellmaitland97@gmail.com>
2023-11-03 15:16:26 -0400

    Test

--- push.ps1
+++ push.ps1
@@ -2,4 +2,3 @@ git add .
 $MSG=Read-Host "Enter a commit message"
 git commit -m $MSG
 git push
-curl http://192.168.254.156:8081/job/Deploy%20Genso/build?token=11ba6d22f6ab9b626f186094f6afb9f243 -UseBasicParsing
